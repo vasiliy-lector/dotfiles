2910f5805c484b734346178673c87fcf623c2597
Vasiliy Lector <vasiliy.lector@gmail.com>
2022-02-07 10:09:35 +0300

    vim-plug for neovim

--- install.sh
+++ install.sh
@@ -58,6 +58,10 @@ cp libxkbswitch-macosx/bin/libxkbswitch.dylib /usr/local/lib
 rm -rf libxkbswitch-macosx
 echo Done
 
+# vim-plug for neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 echo Next you need to do:
 echo "1) Set iTerm2 settings: Keys -> Remap Modifier Keys -> Right command to Control"
 echo "2) Set Mac OS settings: System Preferences -> Keyboard -> Modifier Keys -> Caps Lock to Control"
