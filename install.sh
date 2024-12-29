cb0d2e43cecd88685f90d9f216f3e505d4d891a8
Vasiliy Dyakov <vasilij.dyakov@gmail.com>
2024-12-29 18:12:20 +0300

    fix install.sh

--- install.sh
+++ install.sh
@@ -9,11 +9,11 @@ done
 mkdir -p ~/.config
 ln -fs $PWD/nvim ~/.config/nvim
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-brew install wget
-brew install antigen
-brew install neovim
-brew install n
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+# brew install wget
+# brew install antigen
+# brew install neovim
+# brew install n
 
 echo Please input git user name
 read name
@@ -27,16 +27,14 @@ cp git-completion.bash ~/git-completion.bash
 cp git-prompt.sh ~/git-prompt.sh
 cp gruvbox_256palette.sh ~/gruvbox_256palette.sh
 
-source ~/.profile
+# source ~/.profile
 
 echo Done
 
 # vim-plug for neovim
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+#        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 echo Next you need to do:
-echo "1) Set iTerm2 settings: Keys -> Remap Modifier Keys -> Right command to Control"
-echo "2) Set Mac OS settings: System Preferences -> Keyboard -> Modifier Keys -> Caps Lock to Control"
-echo "3) To fix repeating keys problem on Mac OS execute: 'defaults write -g ApplePressAndHoldEnabled -bool false'"
-echo "4) Download, install and set as default for iTerm2 following font: https://github.com/powerline/fonts/blob/master/Meslo%20Slashed/Meslo%20LG%20M%20Regular%20for%20Powerline.ttf"
+echo "1) To fix repeating keys problem on Mac OS execute: 'defaults write -g ApplePressAndHoldEnabled -bool false'"
+echo "2) Download, install and set as default for iTerm2 following font: https://github.com/powerline/fonts/blob/master/Meslo%20Slashed/Meslo%20LG%20M%20Regular%20for%20Powerline.ttf"
